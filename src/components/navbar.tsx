cbcbe9f379d0f903b28d7b12dc93d9759f4380b3
Yogesh <yogi.2210@gmail.com>
2022-12-19 15:46:09 +0530

    add logout functionality

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { auth } from '../config/firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { signOut } from 'firebase/auth'
 
 export const Navbar = () => {
     const [ user ] = useAuthState(auth)
+
+    const signUserOut = async () =>{
+        await signOut(auth)
+    }
   return (
     <div className='navbar' >
         <div className='navbar-links' >
@@ -14,8 +19,13 @@ export const Navbar = () => {
         </div>
 
         <div className='navbar-user-info' >
+            {user && 
+            <>
             <p>{user?.displayName}</p>
             <img src= {user?.photoURL || "img"} width='100' height='100' alt=""/>
+            <button onClick={signUserOut} >Log Out</button>
+            </>
+            }
         </div>
 
     </div>
